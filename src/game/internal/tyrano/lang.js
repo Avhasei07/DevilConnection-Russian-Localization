ebece49079b7ac753ac91738342dbfec576dc0f9
Rushan Mukhutdinov <firvelfrivol@gmail.com>
2025-11-06 02:02:44 +0300

    Add Russian localization for DevilConnection
    Initial commit with Russian localization assets, translated strings, documentation, and support files for the DevilConnection game and manga. Includes localized images, README, changelog, license, and translation mappings for both the game and comic adaptation.

---
+++ src/game/internal/tyrano/lang.js
@@ -0,0 +1,88 @@
+window.tyrano_lang = {
+  word: {
+    go_title: 'Вернуться в главное меню?',
+    close_confirm: 'Фуах… Пожалуй, пора отдохнуть.',
+    load_data: 'Загрузить этот файл сохранения?',
+    load_data_kill:
+      '<span style="color: red;font-family: CormorantInfant-SemiBold">Загрузить эту запись (это сохранение)</span>',
+    load_auto_save:
+      'Загрузить запись автосохранения?<br>Время последнего автосохранения:<br>$time',
+    load_auto_save_tutorial:
+      'Нажми в правом верхнем углу на <span style="color:#2ea6b7;">«Вернуться чуть назад»</span><br>и начни заново с последнего момента',
+    overwrite_save: 'Перезаписать и сохранить?',
+    overwrite_save_kill:
+      '<span style="color: red;font-family: CormorantInfant-SemiBold">Перезаписать и сохранить?</span>',
+    stop_save: 'Нельзя сохраняться в таком месте',
+    exit_game: 'Закрыть окно и выйти из игры?',
+    not_saved: 'СОХРАНЕНИЙ НЕТ',
+    delete_photo: 'Действительно удалить эту фотографию?',
+    save_photo: 'Выбери папку для сохранения',
+    quit_deco: 'Закончить украшение и вернуться в альбом?',
+    delete_all_data:
+      'Записи коллекции будут сохранены,<br>но всё остальное будет откатано назад. Вы уверены?',
+    delete_all_data_confirm:
+      '<span style="text-decoration: underline">Последнее подтверждение</span><br>Файлы сохранения нельзя будет восстановить после удаления.<br>Если всё равно продолжить, нажми «OK».',
+    delete_all_data_kill_1:
+      '<span style="color: red;font-family: CormorantInfant-SemiBold">Я никогда не позволю тебе сбежать</span>',
+    delete_all_data_kill_2:
+      '<span style="color: red;font-family: CormorantInfant-SemiBold">Теперь уже слишком поздно</span>',
+    delete_all_data_kill_3:
+      '<span style="color: red;font-family: CormorantInfant-SemiBold">Эй, сделай это ещё раз</span>',
+    album_full: 'В альбоме больше нет свободного места.<br>Удали несколько фотографий.',
+    tag: 'Тег',
+    not_exists: 'не существует',
+    error: 'Произошла ошибка. Проверь сценарий.',
+    label: 'Метка',
+    label_double: 'дублируется в том же файле сценария',
+    error_occurred: 'Произошла ошибка',
+    save_file_violation_1:
+      'Вы подделали файл сохранения? Не запускайте игру с сохранением, которое не соответствует вашему прохождению.',
+    save_file_violation_2: 'Можно загрузить этот файл сохранения и запустить игру?',
+    save_file_violation_3:
+      'Запуск отменён. Удали файл сохранения и попробуй снова.',
+    steam_login_failed: 'Запусти игру через Steam',
+    lord: 'Рад, что встретил тебя',
+    reset_devi: 'Что ты в облаках витаешь?<br>Пошли скорее забирать энергию.',
+    double_start: 'Уже запущено.',
+    reload: 'Перезагрузить',
+    yes: 'Да',
+    no: 'Нет',
+    nod: 'Кивнуть',
+    silent: '…',
+    ok: 'Ага',
+    cancel: 'Отмена',
+    collection: {
+      chara: {
+        beast: 'Магзвери',
+        debirun: 'Дэбирун',
+        demon: 'Демоны и падшие ангелы',
+        fairy: 'Духи и ангелы',
+      },
+      ending: {
+        normal: 'Концовки',
+        secret: 'Тайны',
+      },
+    },
+    subtitle: {
+      name: '~ В поисках истинного имени ~',
+      end: '~ В поисках развязки ~',
+      kanou: '~ В поисках возможностей ~',
+      bel: '~ В поисках Бельфегора ~',
+    },
+    kill_load: 'Никогда не прощу',
+  },
+
+  novel: {
+    file_menu_button_save: 'menu_button_save.gif',
+    file_menu_button_load: 'menu_button_load.gif',
+    file_menu_button_message_close: 'menu_message_close.gif',
+    file_menu_button_skip: 'menu_button_skip.gif',
+    file_menu_button_title: 'menu_button_title.gif',
+    file_menu_button_close: 'menu_button_close.png',
+    file_menu_bg: 'menu_bg.jpg',
+    file_save_bg: 'menu_save_bg.jpg',
+    file_load_bg: 'menu_load_bg.jpg',
+    file_button_menu: 'button_menu.png',
+    error_occurred: 'Произошла ошибка',
+  },
+}
